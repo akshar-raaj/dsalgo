085895c8a06d6acc69460f2ea4f69df28c4f240a
Akshar Raaj <akshar@urbanpiper.com>
2020-10-21 00:42:17 +0530

    fix

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 def test_one():
     assert "hello" == "hello"
 def test_two():
-    assert "hello world" == "hello world "
+    assert "hello world" == "hello world"
 if __name__ == '__main__':
     test_one()
     test_two()
